6d4a1df9afc5af667b0de1da6b8aa7ca5fcce712
Vasiliy Teplov <>
2022-07-07 20:05:38 +0300

    first commit

---
+++ TheOneApp/TheOneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheOneAppApp.swift
+//  TheOneApp
+//
+//  Created by Vasilii Teplov on 07.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TheOneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
